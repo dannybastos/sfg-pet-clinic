c3bd6e95664f0e5c321115058e8899de7c5365b0
Danny Bastos <danny.bastos.br@gmail.com>
2018-11-12 20:29:21 -0200

    fixed vets controller and page

--- pet-clinic-web/src/main/java/br/com/spring5/petclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/br/com/spring5/petclinic/controllers/VetsController.java
@@ -1,14 +1,28 @@
 package br.com.spring5.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.spring5.petclinic.model.Vet;
+import br.com.spring5.petclinic.service.VetsService;
 
 @Controller
 public class VetsController {
 
+	private static final String VETS_VIEW = "vets/vets";
+	private VetsService vetService;
+	
+	public VetsController(VetsService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@GetMapping({"vets"})
-	public String listVets() {
-		return "vets/vets";
+	public String listVets(Model model) {
+		Iterable<Vet> vetList = this.vetService.findAll();
+		model.addAttribute("vetList", vetList);
+		return VETS_VIEW;
 	}
 	
 }
