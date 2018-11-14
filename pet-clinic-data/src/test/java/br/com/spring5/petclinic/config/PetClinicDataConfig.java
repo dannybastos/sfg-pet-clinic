1a5ee3b94b8eba9179364d0dbbc39e09b5b7ef18
Danny Bastos <danny.bastos.br@gmail.com>
2018-11-13 22:59:01 -0200

    fixed spring data version;
    created application-dev.properties;
    fixed configuration issues

--- pet-clinic-data/src/test/java/br/com/spring5/petclinic/config/PetClinicDataConfig.java
+++ pet-clinic-data/src/test/java/br/com/spring5/petclinic/config/PetClinicDataConfig.java
@@ -3,8 +3,11 @@ package br.com.spring5.petclinic.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages= {"br.com.spring5.petclinic"})
-public class PetClinicDataConfig {
+/**
+ * Created by danny on 13/11/18.
+ */
 
+@Configuration
+@ComponentScan(basePackages= {"br.com.spring5"})
+public class PetClinicDataConfig {
 }
--- pet-clinic-web/src/main/java/br/com/spring5/petclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/br/com/spring5/petclinic/SfgPetClinicApplication.java
@@ -3,7 +3,7 @@ package br.com.spring5.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="br.com.spring5.petclinic")
 public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
